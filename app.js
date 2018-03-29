8c0c9852181800f9e4200e7e965c34da20aadaf7
ArtemGermanenko <artemgermanenko67@gmail.com>
2018-03-29 19:16:15 +0300

    Server task7

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const serveStatic = require('serve-static');
 
 const app = express();
 
-app.use(serveStatic('public/task6/'));
+app.use(express.static('public/task6/'));
 
 const server = app.listen(3030, () => {
 	console.log(`Server on port ${server.address().port}`);
