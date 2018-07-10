7ccfb4239d08b89faf468d1877f829be446414ef
ArtemGermanenko <artemgermanenko67@gmail.com>
2018-07-10 14:06:10 +0300

    v12.0.3

---
+++ README.md
@@ -0,0 +1,3 @@
+**API**
+***
+     dir/fonts *GET /asdasd 
--- app.js
+++ app.js
@@ -5,6 +5,18 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const controller = require('./server/Controller');
 const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var Cat = mongoose.model('Cat', {name: String});
+var kitty = new Cat({name: "lol"});
+kitty.save(function(err){
+  if(err){
+    console.log(err);
+  } else {
+    console.log('meow');
+  }
+});
 
 const pathPosts = 'server/data/posts.json';
 const pathUsers = 'server/data/users.json';
